e684c9704a3fee9ef1fe4d31bcb083c7a6c3540b
Matadysa <matadysa@gmail.com>
2024-04-16 11:54:30 +0330

    done

--- TFLAHW1/pythonProject/main.py
+++ TFLAHW1/pythonProject/main.py
@@ -1,24 +1,6 @@
 import hashlib
 
 
-def hash_file(file_path, hash_algorithm='SHA256'):
-    # Choose the hashing algorithm
-    hash_function = hashlib.new(hash_algorithm)
-
-    # Open the file in binary mode
-    with open(file_path, 'rb') as file:
-        # Read the file content in chunks
-        chunk_size = 4096
-        while chunk := file.read(chunk_size):
-            # Update the hash object with the chunk
-            hash_function.update(chunk)
-
-    # Get the hexadecimal digest of the hash
-    file_hash = hash_function.hexdigest()
-
-    return file_hash
-
-
 def hash_string(input_string):
     # Create a sha256 hash object
     sha_signature = hashlib.sha256(input_string.encode('utf-8')).hexdigest()
@@ -30,7 +12,11 @@ f = open("ans.txt", "r")
 print(repr(f.read()))
 print(hash_string(f.read()))
 print(hash_string(
-    '0:\n1 0\n1:\n2 3\n2:\n4 2\n3:\n4 5\n4:\n2 1\n5:\n0 3\n1 4\nA:1\nB:'))
+    '0:\n1 0\n1:\n2 3\n2:\n4 2\n3:\n4 5\n4:\n2 1\n5:\n0 3\n1 4\nA:10\nB:'))
+print(hash_string(
+    '0:\n1 0\n1:\n2 3\n2:\n4 2\n3:\n4 5\n4:\n2 1\n5:\n0 3\n1 4\nA:10\nB:\n'))
+print(hash_string(
+    '0:\n1 0\n1:\n2 3\n2:\n4 2\n3:\n4 5\n4:\n2 1\n5:\n0 3\n1 4\nA:10\nB:') == '6797e56802e0ff2f5fff1165063f6b8daa5f02b93ed6b905f35e530f82c5661b')
 print(hash_string(f.read()) == '6797e56802e0ff2f5fff1165063f6b8daa5f02b93ed6b905f35e530f82c5661b')
 print(hash_string(
-    '0:\n1 0\n1:\n2 3\n2:\n4 2\n3:\n4 5\n4:\n2 1\n5:\n0 3\n1 4\nA:1\nB:\n') == '6797e56802e0ff2f5fff1165063f6b8daa5f02b93ed6b905f35e530f82c5661b')
+    '0:\n1 0\n1:\n2 3\n2:\n4 2\n3:\n4 5\n4:\n2 1\n5:\n0 3\n1 4\nA:10\nB:\n') == '6797e56802e0ff2f5fff1165063f6b8daa5f02b93ed6b905f35e530f82c5661b')
